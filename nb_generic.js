93b0a0ad57a1d9cbf95cd3b7c03f1a7f4f440dba
haoNoQ <noqnoqneo@gmail.com>
2013-01-22 09:38:14 +0400

    Fix typos found by Duha

--- nb_generic.js
+++ nb_generic.js
@@ -11,7 +11,7 @@
 
 // You can redefine these paths when you make a customized AI
 // for a map or a challenge.
-NB_PATH = "/multiplay/skirmish/" ;
+NB_PATH = "/multiplay/skirmish/";
 NB_INCLUDES = NB_PATH + "nb_includes/";
 NB_RULESETS = NB_PATH + "nb_rulesets/";
 
