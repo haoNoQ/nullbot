5a2e5f8a2850d1f31dabef65b64e374a47e2f911
Commander Manuel <nmsputnick559@gmail.com>
2015-07-23 16:54:27 -0500

    Add a custom Harold for Experimentation
    I barely understand what all these lines of code do. If only @haoNoQ
    could tell me!

---
+++ har_includes-custom/_main.js
@@ -0,0 +1,26 @@
+
+/*
+ * This file connects all remaining pieces of AI code together.
+ * It shouldn't contain any code itself.
+ * 
+ * NOTE: order matters!
+ * 
+ */
+
+var personality;
+
+include(HAR_INCLUDES + "math.js");
+include(HAR_INCLUDES + "intensity.js");
+include(HAR_INCLUDES + "misc.js");
+include(HAR_INCLUDES + "stats.js");
+
+include(HAR_INCLUDES + "adapt.js");
+include(HAR_INCLUDES + "build.js");
+include(HAR_INCLUDES + "research.js");
+include(HAR_INCLUDES + "produce.js");
+include(HAR_INCLUDES + "tactics.js");
+include(HAR_INCLUDES + "lassat.js");
+include(HAR_INCLUDES + "chat.js");
+
+include(HAR_INCLUDES + "timers.js");
+include(HAR_INCLUDES + "_events.js");
